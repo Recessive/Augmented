aa6eeedaa069f9a00a8586ca1deb60e8d394f59a
Recessive <sean.gribben5@gmail.com>
2023-07-23 23:04:09 +1000

    Fixed the room change system

--- RoomControl.gd
+++ RoomControl.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export
+var player : Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,16 +9,23 @@ func _ready():
 	
 	pass # Replace with function body.
 
-
+var make_room : Room = null
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if make_room:
+		var dir = DirAccess.open(make_room.path)
+		var files = dir.get_files()
+		# Assuming equal weighting of each room here
+		var fname = files[randi_range(0, files.size()-1)]
+		var next_room = load(make_room.path + fname).instantiate()
+		next_room.new_room.connect(new_room)
+		add_child(next_room)
+		make_room = null
 
 func new_room(room : Room):
-	remove_child(get_child(0))
-	var dir = DirAccess.open(room.path)
-	var files = dir.get_files()
-	# Assuming equal weighting of each room here
-	var fname = files[randi_range(0, files.size()-1)]
-	var next_room = load(room.path + fname).instantiate()
-	add_child(next_room)
+	player.position = Vector2(0, 0)
+	get_child(0).queue_free()
+	make_room = room
+	# Make the room on the next frame to prevent double collisions
+	
+	
