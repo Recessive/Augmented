46b81891600401c4e7242298a846a3ac42a5cf08
Recessive <sean.gribben5@gmail.com>
2023-07-24 11:19:49 +1000

    Updated player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,18 +3,24 @@ extends CharacterBody2D
 @export
 var SPEED : float
 
+@export
+var ACCELERATION : float
 
 func _physics_process(delta):
 	
-	velocity = Vector2(0, 0)
+	
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direc = Vector2(0, 0)
 	var lr = Input.get_axis("ui_left", "ui_right")
 	var ud = Input.get_axis("ui_up", "ui_down")
 	if lr:
-		velocity.x = lr * SPEED
+		direc.x += lr
 	if ud:
-		velocity.y = ud * SPEED
-
+		direc.y += ud
+	
+	direc = direc.normalized()
+	
+	velocity = velocity.move_toward(direc * SPEED, ACCELERATION * delta)
 	move_and_slide()
