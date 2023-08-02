d8367f75953dc54807d3bc850f93a4855cb22ee9
milktheinternet <122847729+milktheinternet@users.noreply.github.com>
2023-08-02 19:08:22 -0400

    Pause menu added!
    press ESC to pause, the resume button is the only one that works

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -4,6 +4,17 @@ var camera : Camera2D
 var screen_size : Vector2
 var healthBar : Node2D
 var roomControl : Node
+var menus : Dictionary
+var activeMenu = 'game'
+
+func hide_menus():
+	for menu in menus.values():
+		menu.hide()
+
+func switch_menu(menu):
+	hide_menus()
+	menus[menu].show()
+	activeMenu = menu
 
 func update_camera():
 	# places the gui on the camera
@@ -12,15 +23,36 @@ func update_camera():
 	size.x = screen_size.x / camera.zoom.x
 	size.y = screen_size.y / camera.zoom.y
 	global_position = camera.global_position - Vector2(size.x/2, size.y/2)
-	print(global_position)
+	# switch to the game window when you enter a new room
+	switch_menu('game')
 
 func _ready():
+	menus = {
+		'game':$Game,
+		'pause':$PauseMenu
+	}
 	healthBar = $Game/TopInfo/Healthbar
 	roomControl = $/root/main/RoomControl
 	update_camera()
 
 func _process(delta):
-	healthBar.updateHP(PlayerStats.hp/PlayerStats.maxHP)
-	var depth = roomControl.depth
-	var heat = roomControl.heat
-	$Game/TopInfo/Label.text = "DEPTH: %s\nHEAT: %s" % [depth, heat]
+	# update the game menu
+	if activeMenu == 'game':
+		healthBar.updateHP(PlayerStats.hp/PlayerStats.maxHP)
+		var depth = roomControl.depth
+		var heat = roomControl.heat
+		$Game/TopInfo/Label.text = "DEPTH: %s\nHEAT: %s" % [depth, heat]
+		
+		if Input.is_action_just_pressed('pause'):
+			switch_menu('pause')
+			get_tree().paused = true
+			
+	elif activeMenu == 'pause':
+		if Input.is_action_just_pressed('pause'):
+			switch_menu('game')
+			get_tree().paused = false
+
+
+func _on_resume_pressed():
+	switch_menu('game')
+	get_tree().paused = false
