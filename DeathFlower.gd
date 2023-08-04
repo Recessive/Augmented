1936c3cd1ccfc5bcf76f20bb2a3b51f71372d9ad
milktheinternet <122847729+milktheinternet@users.noreply.github.com>
2023-08-04 10:11:02 -0400

    Death flower now appears to deal damage

--- DeathFlower.gd
+++ DeathFlower.gd
@@ -1,9 +1,10 @@
 extends "res://Scripts/Base/EnemyBase.gd"
 
-
+func _ready():
+	CONTACT_DAMAGE = 10
 
 func _on_area_2d_body_entered(body):
-	
+	contact_damage(body,(body.position-position)*Vector2(10,10))
 	explode()
 
 func explode():
