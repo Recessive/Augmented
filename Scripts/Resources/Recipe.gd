33f86260ce4755e2b825f442c1c4bdf21f4256ca
Recessive <sean.gribben5@gmail.com>
2023-08-05 01:55:24 +1000

    Inventory and recipe system implemented

--- Scripts/Resources/Recipe.gd
+++ Scripts/Resources/Recipe.gd
@@ -2,4 +2,29 @@ extends Resource
 
 class_name Recipe
 
-# @export
+@export
+var tier1requirements : Dictionary
+
+@export
+var tier2requirements : Dictionary
+
+# Head
+# Body
+# Legs
+# Arms
+@export
+var bodyPart : String
+
+@export
+var productName : String
+
+func can_afford():
+	for key in tier1requirements:
+		if !PlayerStats.tier1Inventory.has(key) or PlayerStats.tier1Inventory[key] < tier1requirements[key]:
+			return false
+	
+	for key in tier2requirements:
+		if !PlayerStats.tier2Inventory.has(key) or PlayerStats.tier2Inventory[key] < tier2requirements[key]:
+			return false
+	
+	return true
